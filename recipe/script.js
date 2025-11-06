cccbded1f9cf80ccc22f26d763dd8269d1866aef
Kacie Dearman <kldearman@amarillocollege.com>
2025-11-05 19:21:44 -0600

    Small change to HTML & added tab functions to js Removed the tab for toppings on small screen. Might add it back later. Added the DOM functions in JS to show the appropriate ingredients and instructions when each tab is tapped or clicked.

--- recipe/script.js
+++ recipe/script.js
@@ -66,4 +66,24 @@ icingListItems.forEach((li, index) => {
 })
 
 
-// Code for @media to show and hide ingredients and instructions on page.
+// Code for tabs and their content to show on small screens
+function openRecipe(evt, recipeName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(recipeName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
