2ef785c02077c1e43806c9507c71810de20baa42
Kacie Dearman <kldearman@amarillocollege.com>
2025-11-26 21:57:40 -0600

    Created JS sudo code and corresponding sections for each function of the Boggle game.

---
+++ games-app/index.js
@@ -0,0 +1,35 @@
+// (data) Create an array of dice (each dice has 6 letters)
+
+
+// (utility) Create a function that "rolls" each dice and returns
+// a random possible letter from each dice
+
+
+
+// (index) Create a function that displays the dice on the board using DOM
+
+
+// Create a function that displays which dice are selected 
+// and adds the letters (in selected order) to the #currentWord
+
+
+// Create a function that checks #currentWord after #submitBtn
+// display word & it's score (3-4: 1; 5: 2; 6: 3; 7: 5; 8+: 11)
+// add word score to total points
+
+
+// Calculate score
+
+
+// Display submitted word, it's score, and recalculate total score
+
+
+
+// Reset Function, resets selections when reset button is clicked
+
+
+///// EXTRAS //////
+// Create a function that checks #currentWord after #submitBtn is 
+// clicked against a dictionary of words.
+
+// Allows selection of ONLY adjacent dice.
