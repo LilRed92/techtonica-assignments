14ff64f3088607cc547433881935c3cb74f566da
Kacie Dearman <kldearman@amarillocollege.com>
2025-12-18 18:54:48 -0600

    Added use states for sequence, userinput, and score to app.jsx. Added styling for app.jsx to app.css, added more node packages to help with development process.

--- color-sequence-game-react/src/App.jsx
+++ color-sequence-game-react/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import Scoreboard from"./components/Scoreboard";
+import Controls from"./components/Controls";
+import GameBoard from"./components/GameBoard";
 import './App.css'
 
+const COLORS = ["red","green","blue","purple"];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [sequence, setSequence] =useState([]);
+  const [userInput, setUserInput] =useState([]);
+  const [score, setScore] =useState(0);
 
   return (
-    <>
+    <div className="app-main">
       <h1>Color Sequence Memory Game (React)</h1>
-    </>
+    </div>
+      
   );
 }
 
