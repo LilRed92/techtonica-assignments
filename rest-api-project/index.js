e6d5d1f789b48d74e988cfad5c90c86399b5f6c5
Kacie Dearman <kldearman@amarillocollege.com>
2026-01-08 00:10:52 -0600

    Npm setup & started on routes + created controllers dir & file
    Setup npm by npm init -y
    Installed express and nodemon
    Modified package.json file for type.
    ""          ""      with scripts
    
    Added routes dir and corresponding file.
    Started working on routes for project
    
    Added controllers dir and corresponding file
    
    Created bestGames.js with the top 120 games scraped from the web.

---
+++ rest-api-project/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import gamesRouter from './routes/gamesRouter.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/bestGames', gamesRouter);
+
+app.get('/', (req, res) => res.send('Hello from homepage.'));
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
